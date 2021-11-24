46e744512864a9120430a1d36eeffaf24d5127d5
Saeid Vahidnia <saeidvn@gmail.com>
2021-11-24 14:03:30 +0300

    add name to store and warehouse

--- src/main/java/com/solvd/store/store/Store.java
+++ src/main/java/com/solvd/store/store/Store.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Store {
 
+    private String name;
     private List<Section> sections;
 
     public List<Section> getSections() {
--- src/main/java/com/solvd/store/store/Warehouse.java
+++ src/main/java/com/solvd/store/store/Warehouse.java
@@ -3,6 +3,7 @@ package com.solvd.store.store;
 public class Warehouse {
 
     private Long id;
+    private String name;
     private Section section;
 
     public Long getId() {
